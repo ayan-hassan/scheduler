1538aa282ca25f5e88fe8e65dcd3914132e50c56
ayan-hassan <ahassan@hotmail.ca>
2023-03-23 02:19:51 -0400

    add conditionals to daylistitem for 0 or 1 spots

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -5,15 +5,30 @@ import "components/DayListItem.scss";
 
 
 export default function DayListItem(props) {
+
   const dayListItemClass = classNames('day-list__item', {
     'day-list__item--selected': props.selected,
     'day-list__item--full': props.spots === 0
- });
- 
+  });
+
+  const formatSpots = function () {
+    let spotsAvailable;
+    if (props.spots === 0) {
+      spotsAvailable = 'no spots remaining';
+    } else if (props.spots > 0) {
+      spotsAvailable = `${props.spots}${props.spots === 1 ? ' spot ': ' spots '} remaining`;
+    }
+    return spotsAvailable;
+  };
+
   return (
-    <li className={dayListItemClass} onClick={() => props.setDay(props.name)}>
-      <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+    <li 
+      data-testid = "day"
+      className = {dayListItemClass}
+      onClick = {() => props.setDay(props.name)}
+    >
+      <h2 className="text--regular">{ props.name }</h2>
+      <h3 className="text--light">{formatSpots()}</h3>
     </li>
   );
-}
+};
