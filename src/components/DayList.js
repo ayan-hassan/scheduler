179ed893de68f631388a629c351892bedd59417f
ayan-hassan <ahassan@hotmail.ca>
2023-03-23 16:56:32 -0400

    build interview list, stories, styling

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -3,9 +3,10 @@ import DayListItem from "./DayListItem";
 
 export default function DayList(props) {
   //dynamically create array of DayListItem components using .map
-  const days = props.days.map( day => {
+  const days = props.days.map(day => {
     return (
-      <DayListItem 
+      <DayListItem
+        key={day.id} 
         name={day.name}
         spots={day.spots}
         selected={day.name === props.day}
