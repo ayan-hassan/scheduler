2be26ffbe550906e36c198177b370978f5302d46
ayan-hassan <ahassan@hotmail.ca>
2023-03-23 22:11:51 -0400

    complete appointment component

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -3,14 +3,14 @@ import DayListItem from "./DayListItem";
 
 export default function DayList(props) {
   //dynamically create array of DayListItem components using .map
-  const days = props.days.map(day => {
+  const days = props.value.map(day => {
     return (
       <DayListItem
         key={day.id} 
         name={day.name}
         spots={day.spots}
-        selected={day.name === props.day}
-        setDay={props.setDay}
+        selected={day.name === props.value}
+        setDay={props.onChange}
       />
     )
   });
--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -23,9 +23,9 @@ export default function DayListItem(props) {
 
   return (
     <li 
-      data-testid = "day"
       className = {dayListItemClass}
       onClick = {() => props.setDay(props.name)}
+      selected={props.selected}
     >
       <h2 className="text--regular">{ props.name }</h2>
       <h3 className="text--light">{formatSpots()}</h3>
